1fe89ac9ecc6cfa754fe82ebc4e2515ed92b3a67
Erhan Akpınar <82037418+erhanakp@users.noreply.github.com>
2021-11-09 21:27:24 +0300

    ges

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 	e.GET("/swagger/*", echoSwagger.WrapHandler)
 
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Helloss, World!")
+		return c.String(http.StatusOK, "Hellosses, World!")
 	})
 	e.Logger.Fatal(e.Start(":1323"))
 }
