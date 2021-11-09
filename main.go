2782b29ef0ce04caed0bddab79f335a19c2f3321
Erhan Akpınar <82037418+erhanakp@users.noreply.github.com>
2021-11-09 20:29:18 +0300

    test

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
+		return c.String(http.StatusOK, "Hellos, World!")
 	})
 	e.Logger.Fatal(e.Start(":1323"))
 }
